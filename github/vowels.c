46a447a7a7183af5eb1a3f558e6225642531b744
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-01-08 16:40:42 +0530

    Create vowels.c

---
+++ github/vowels.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char L;
+printf("Enter the letter to be checked for vowel or consonant");
+scanf("%c",&L);
+if(L==a||L==e||L==i||L==o||L==u)
+{
+printf("The word given is a vowel");
+}
+else
+{
+printf("The word given is a consonant");
+}
+}
