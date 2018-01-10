0cdcfa77a18b1b143c4582dd73e6bb2c37f1e040
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-01-10 16:25:03 +0530

    Create sum.c

---
+++ github/sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int n,sum;
+printf("enter the number of natural numbers");
+scanf("%d",&n);
+sum=(n*(n+1)/2);
+printf("the sum of %d natural number is %d",n,sum);
+}
