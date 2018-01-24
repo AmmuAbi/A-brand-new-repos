9930572baa7401c312c079416ab3e28ca9f6665c
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-01-24 16:10:21 +0530

    Create swap1.c

---
+++ github/swap1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int s,a,l;
+printf("enter the values of the two digits");
+scanf("%d%d",&s,&a);
+printf("the value of the numbers are s=%d,a=%d",s,a);
+s=l;
+l=a;
+a=s;
+printf("the value of numbers are s=%d,a=%d",s,a);
+}
