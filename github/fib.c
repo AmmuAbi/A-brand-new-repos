9bfdcd20169321d0b38c6c21101905862ef3eaf6
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-01-24 16:43:47 +0530

    Create fib.c

---
+++ github/fib.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int f=0;s=1;v,limit,i;
+printf("enter the limit");
+scanf("%d"&limit);
+printf("%d%",f,s);
+for(i=2;i<l;i++)
+{
+v=f+s;
+f=s;
+s=v;
+printf("%d",v);
+}
+}
