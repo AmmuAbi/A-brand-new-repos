bc5d0f623afaf3245da31e43dc7d1d89545ecb84
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-01-28 14:33:51 +0530

    Create less.c

---
+++ github/less.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int n,j;
+    printf("enter the no");
+    scanf("%d",&n);
+    for(j=1;j<=10;j++)
+    {
+        if(n<=10)
+        {
+        printf("yes\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
+    }
+}
