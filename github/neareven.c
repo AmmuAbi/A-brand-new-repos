47610deb6a5b3c7e7ba9f74fc7fa084ea4fa5fa2
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-01-31 16:56:16 +0530

    Create neareven.c

---
+++ github/neareven.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("enter the number");
+printf("%d",&num);
+if(num%2==0)
+{
+printf("no is %d",num-2);
+}
+else
+{
+printf("no is %d",num-1);
+}
+}
