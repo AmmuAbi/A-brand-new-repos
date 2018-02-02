bd781f35ba0929d7347be295fe2faad17413baba
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-02-02 16:30:58 +0530

    Update max.c

--- github/max.c
+++ github/max.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 void main()
 {
-int a[50],i,max,size;
-printf("enter the array size");
-scanf("%d",&size);
+int a[50],i,max,size=10;
 for(i=0;i<size;i++)
 {
 scanf("%d",&a[i]);
@@ -16,5 +14,5 @@ if(a[i]>max)
 max=a[i];
 }
 }
-printf("the maximum element is %d",min);
+printf("the maximum element is %d",max);
 }
