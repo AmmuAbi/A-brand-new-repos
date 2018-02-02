407422f96d25f1363931a32324418df7ac60dd80
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-02-02 16:10:45 +0530

    Create charalnum.c

---
+++ github/charalnum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+{
+char a[10];
+scanf("%s",&a);
+	if((a[10]>='a')&&(a[10]<='z')&&(a[10]>='A')&&(a[10]<='Z')&&(a[10]>='0')&&(a[10]<='9'))
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+    }
+}
