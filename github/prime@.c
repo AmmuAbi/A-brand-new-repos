a02f102dbe313bc66fe3290a043c659d6a41e3e7
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-02-05 15:35:50 +0530

    Rename prime.c to prime@.c

---
+++ github/prime@.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int s,i;
+int flag=0;
+printf("enter the number to be checked for prime");
+scanf("%d",s);
+for(i=2;i<=s/2;i++)
+{
+if(s%i==0)
+{
+flag=1;
+}
+}
+if(flag==0)
+{
+printf("prime");
+}
+}
