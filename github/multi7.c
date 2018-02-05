67535bb3f6afec22adc56666926039f782cb7f4d
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-02-05 15:51:16 +0530

    Create multi7.c

---
+++ github/multi7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("enter the number to be checked for multiple of 7");
+scanf("%d",&num);
+if(num%7==0)
+{
+printf("it is a multiple");
+}
+else
+{
+printf("no");
+}
+}
