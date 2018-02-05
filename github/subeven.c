432be96e57f8a1040e47627f5ee88bf45ea14fe2
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-02-05 16:01:51 +0530

    Update subeven.c

--- github/subeven.c
+++ github/subeven.c
@@ -6,7 +6,7 @@ int a,s,sub;
 printf("entre the two nmbers");
 scanf("%d%d",&a,&s);
 sub=a-s;
-printf("the sum of two numbers is %d",sub);
+printf("the difference of two numbers is %d",sub);
 if(sub%2==0)
 {
 printf("even");
