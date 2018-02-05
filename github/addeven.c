32baa7b432337107c810a90a869a2d9f85cd6f42
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-02-05 15:42:31 +0530

    Create addeven.c

---
+++ github/addeven.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,s,add;
+printf("entre the two nmbers");
+scanf("%d%d",&a,&s);
+add=a+s;
+printf("the sum of two numbers is %d",add);
+if(add%2==0)
+{
+printf(even);
+}
+else
+{
+printf("odd");
+}
+}
