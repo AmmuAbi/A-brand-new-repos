be90c746e743be79d1408ad814a8a99f8a847150
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-02-07 16:16:35 +0530

    Create binary.c

---
+++ github/binary.c
@@ -0,0 +1,15 @@
+#include<conio.h>
+void main()
+{
+char a[100],i;
+printf("enter the string");
+scanf("%s",&a);
+if(a[i]=='1'||a[i]=='0')
+{
+printf("yes it is in binary representation");
+}
+else
+{
+printf("not binary");
+}
+}
