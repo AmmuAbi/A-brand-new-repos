200ea255754b44227247832605230c4e991e4010
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-02-07 16:56:50 +0530

    Create strprint.c

---
+++ github/strprint.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char a[100]:
+int n,i;
+printf("enter the string");
+scanf("&s",&a);
+printf("enter the limit");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("%s",a[i]);
+}
+}
