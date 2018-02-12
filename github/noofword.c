e1b4097beadde604ddcb6ba851c97ab2a36b18f5
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-02-12 15:36:02 +0530

    Update and rename noofwords.c to noofword.c

--- github/noofword.c
+++ github/noofword.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include<string.h>
 void main()
 {
 	char b[100];
 	int len,i,word=1;
 	printf("ENTER A STRING: ");
-	scanf("%d",&b);
+	scanf("%s",&b);
 	len=strlen(b);
 	for(i=0;i<len;i++)
 	{
-		if(b[i]!=' ')
+		if(b[i]==' ')
 			word=word+1;
 	}
 	printf("\n%d",word);
