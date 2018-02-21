36766bdd2bdee011049e9b81687f4b5d7700643e
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-02-21 16:56:14 +0530

    Update and rename cuboid.c to cubo.c

--- github/cubo.c
+++ github/cubo.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-  float l,b,h,vol;
+  float l,b,h,vol,area;
   printf("enter the length breadth and height of the cuboid");
   scanf("%f%f%f",&l,&b,&h);
+  area=l*b;	
   vol=l*b*h;
+  printf("the area of the cuboid is %f",area);	
   printf("the volume of cuboid is %f",vol);
 	return 0;
 }
