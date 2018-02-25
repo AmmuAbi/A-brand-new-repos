f569f0f268797e72eaef82805ffac51e8c46122b
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-02-25 15:09:36 +0530

    Create nk.c

---
+++ github/nk.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int j,n,k,a[100];
+	printf("Enter the n and k value\n");
+	scanf("%d%d",&n,&k);
+	for(j=0;j<n;j++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("%d",a[k-1]);
+	return 0;
+}
