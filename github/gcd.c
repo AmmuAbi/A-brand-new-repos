0fa56656ad3734d798cd9c13f80553bdab576534
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-02-28 16:14:09 +0530

    Update gcd.c

--- github/gcd.c
+++ github/gcd.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-	int a,b,i,ans;
+	int a,b,i,ans,g;
 	printf("enter the two numbers\n");
 	scanf("%d%d",&a,&b);
 	for(i=1;i<=a&&i<=b;i++)
 	{
 		if((a%i==0)&&(b%i)==0)
 		{
-			ans=i;
+			g=i;
 		}
 	}
+	ans=(a*b)/g;
 	printf("the gcd value is :%d",ans);
 	return 0;
 }
