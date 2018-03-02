8593b96f537995623f494b688b4d440243e9e710
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-03-02 16:06:52 +0530

    Update ap.c

--- github/ap.c
+++ github/ap.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-int a,s,d,i,val,result=0;
+int a,s,d,i,val,res=0;
 printf("enter the first term and total number of terms");
 scanf("%d%d",&a,&s);
 printf("enter the common difference");
@@ -10,8 +10,8 @@ val=a;
 for(i=0;i<s;i++)
 {
 printf("%d",i);
-result+=val;
+res+=val;
 val=val+d;
 }
-printf("sum of ap series &d:",result);
+printf("sum of ap series &d:",res);
 }
