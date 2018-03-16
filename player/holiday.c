1c69833328d48a5277edf7972a2957265a0d4647
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-03-16 16:14:59 +0530

    Create holiday.c

---
+++ player/holiday.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char a[10];
+	scanf("%s",a);
+            if(a[0]=='s'||a[0]=='S')
+            {
+            	printf("yes");
+            }
+            else
+            {
+            	printf("no");
+            }
+	return 0;
+}
