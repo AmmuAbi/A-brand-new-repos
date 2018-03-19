3b6ecd72a02abbd67a54ff4fa10fe098cd5a622c
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-03-19 16:07:12 +0530

    Create tri.c

---
+++ player/tri.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,c,k=0;
+	scanf("%d%d%d",&a,&b,&c);
+	if(a<180&&b<180&&c<180)
+	{
+		if((a+b+c)==180)
+		{
+			k=k+1;
+		}
+	}
+	if(k>0)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
