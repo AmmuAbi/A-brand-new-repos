75430bb3010c7018883e40f4d3bc2eb42a41de8b
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-03-19 16:53:00 +0530

    Create pos.c

---
+++ player/pos.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char a[10],b[10];
+	int i,n,c=0;
+	scanf("%s",a);
+	scanf("%s",b);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==b[0])
+		{
+		       c=i;
+			break;
+		}
+	}
+	if(c==i)
+	{
+			printf("%d",c);
+	}
+	return 0;
+}
