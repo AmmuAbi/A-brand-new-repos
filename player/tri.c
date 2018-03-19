dda816603fedcb1762f166b524640fa4ee8009ec
AmmuAbi <31499274+AmmuAbi@users.noreply.github.com>
2018-03-19 16:08:46 +0530

    Update tri.c

--- player/tri.c
+++ player/tri.c
@@ -3,12 +3,15 @@
 int main(void) {
 	int a,b,c,k=0;
 	scanf("%d%d%d",&a,&b,&c);
+	if(a!=0&&b!=0&&c!=0)
+	{
 	if(a<180&&b<180&&c<180)
 	{
 		if((a+b+c)==180)
 		{
 			k=k+1;
 		}
+	}
 	}
 	if(k>0)
 	{
